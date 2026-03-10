c3cafb4de77891bbafc706331f165d793130f6b2
张世冬 <shidongzhang@xyz10.com>
2026-03-10 14:22:16 +0800

    refactor(deploy): 添加 sudo 权限以启动 Docker 和 Nginx
    - 在 docker compose 命令前添加 sudo，以提升权限
    - 在 nginx 命令前添加 sudo，以确保有足够的权限启动 Nginx 配置

--- deploy/start.sh
+++ deploy/start.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-docker compose -f ./docker-compose.yml up -d
+sudo docker compose -f ./docker-compose.yml up -d
 
 # 启动后端
 pnpm run server:run
 
 # 启动前端
-nginx -c ${PWD}/deploy/nginx.conf
+sudo nginx -c ${PWD}/deploy/nginx.conf
